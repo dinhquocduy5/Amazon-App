977d8d1ca1ca3ef466335892b2e1fe0f659f4030
Dinh Quoc Duy <dinhquocduy5@gmail.com>
2021-09-07 22:08:52 +0700

    finish header

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Hello Home Page</h1>
+          </Route>
+        </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css"
+import SearchIcon from "@material-ui/icons/Search"
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+function Header() {
+    return (
+        <div className="header">
+            {/*logo on the left */}
+            <Link to="/">
+                <img className="header__logo" 
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                alt="amazon_logo" />
+            </Link>
+
+            {/*Search box */}
+            <div className="header__search">
+                <input type="text" className="header__searchBox" />
+                <SearchIcon className="header__searchIcon" style={{ fontSize: 40 }} />
+            </div>
+
+            {/* Links */}
+            <div className="header__nav">
+                <Link to="/login"
+                className="header__link">
+                    <div className="header__option">
+                        <span className="title__link">Hello Duy,</span>
+                        <span className="primary__link">Sign in</span>
+                    </div>
+                </Link>
+
+                <Link to="/" 
+                className="header__link">
+                    <div className="header__option">
+                        <span className="title__link">Return</span>
+                        <span className="primary__link">& Orders</span>
+                    </div>
+                </Link>
+            </div>
+
+            <Link to="/checkout" className="header__link">
+            <div className="header__shoppingCart">
+                <ShoppingCartIcon className="header__cartIcon" style={{fontSize: 35}}/>
+                <span className="header__quantity">0</span>
+            </div>
+            </Link>
+        </div>
+    )
+}
+
+export default Header
